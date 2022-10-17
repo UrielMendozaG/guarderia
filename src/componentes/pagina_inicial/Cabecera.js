8aa16f07a0fba9caf555509b857dfe06cfa8e72e
David I <a17100641@gmail.com>
2022-10-16 21:37:19 -0500

    configuracion inicial

---
+++ src/componentes/pagina_inicial/Cabecera.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../../imagenes/logos/logo_perfiles.png';
+/*Componenente que carga el logo 
+y el boton de inicio de sesion*/
+//#region logo de la guarderia
+export const Cabecera = () => {
+  return (
+    <div className='logo'>
+      <img 
+        className='logo-perfiles'
+        src={logo}/>
+        
+    </div>
+  )
+}
+//#endregion
