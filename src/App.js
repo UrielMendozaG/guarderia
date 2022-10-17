d74fa5b5afcdefee4e346757bb150263aae513bc
Victor_Mendoza <victor_mendoza@epam.com>
2022-10-16 21:46:32 -0500

    cambios

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import './componentes/pagina_inicial/Cabecera';
 import { Cabecera } from './componentes/pagina_inicial/Cabecera';
+import { Mensaje } from './componentes/pagina_inicial/Mensaje';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <div className='contenedor-cabecera'>
           <Cabecera/>
+          <Mensaje texto="uriel estuvo aqui"/> 
         </div>
       </header>
     </div>
